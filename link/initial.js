337a356e6917207c97877406c79b440d5e5c51c2
seabye <seabye@live.com>
2020-10-02 12:08:41 +0800

    ...

--- link/initial.js
+++ link/initial.js
@@ -93,7 +93,7 @@
         if('serviceWorker'in window.navigator)window.navigator.serviceWorker.register(path);
     };
     // head
-    export let head=option=>{
+    export let head=(option=null)=>{
         window.document.head.insertAdjacentHTML('afterbegin',
 `<meta name="viewport" content="width=device-width,user-scalable=no,viewport-fit=cover">
 <meta name="format-detection" content="address=no,email=no,telephone=no">
