7b294e76acf977557b62bee8145f04ca0866428c
seabye <seabye@live.com>
2020-10-19 00:31:06 +0800

    ...

--- link/initial_before.js
+++ link/initial_before.js
@@ -3,23 +3,17 @@
     let white='';
     let black='';
     for(const item of window.document.scripts){
-        if(item.dataset.initial===''){
-            if(item.dataset.initial_background_color){
-                const color=item.dataset.initial_background_color.split(',');
-                white=color[0];
-                black=color[1];
-            }
+        if(item.dataset.initial){
+            const data=window.JSON.parse(item.dataset.initial.replace(/'/g,'"'));
             const element=window.document.createElement('script');
-            element.setAttribute('src',item.outerHTML.match(/src="([^"]+)"/)[1].replace('initial_before.js','script.js'));
+            element.setAttribute('src',data.script);
             element.setAttribute('type','module');
             item.insertAdjacentElement('afterend',element);
+            white=data.white?data.white:'#FFFFFF';
+            black=data.black?data.black:'#000000';
             item.parentNode.removeChild(item);
             break;
         }
-    }
-    if(!white){
-        white='#FFFFFF';
-        black='#000000';
     }
     window.document.documentElement.style.setProperty('background-color',`${window.matchMedia('(prefers-color-scheme:dark)').matches?black:white}`);
 }
