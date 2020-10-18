731ed4ab70c34c97ba29636b3984ae33bdba4995
seabye <seabye@live.com>
2020-10-18 19:30:36 +0800

    ...

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-import{initial_sw,initial_head}from'./link/initial.js';
-initial_sw('./sw.js');
-initial_head({title:'seabye',style:'./style.css',icon:'./base/icon.png',icon_apple:'./base/icon-apple.png',manifest:'./manifest.webmanifest'});
+import{initial_sw,initial_head}from'/link/initial.js';
+initial_sw('/sw.js');
+initial_head({title:'seabye',style:'/style.css',icon:'/base/icon.png',icon_apple:'/base/icon-apple.png',manifest:'/manifest.webmanifest'});
 window.document.body.insertAdjacentHTML('beforeend','Hello, World!');
