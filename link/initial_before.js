03c8d4e5a4a9c6798d4ff4151a7c3c45fbb2b036
seabye <seabye@live.com>
2020-10-20 00:09:34 +0800

    ...

--- link/initial_before.js
+++ link/initial_before.js
@@ -1,13 +1,15 @@
 'use strict';
 for(const item of window.document.scripts){
     if(item.dataset.initial){
+        // background color
         const data=window.JSON.parse(item.dataset.initial.replace(/'/g,'"'));
         window.document.documentElement.style.setProperty('background-color',`var(--ic_ve_color_white,${window.matchMedia('(prefers-color-scheme:dark)').matches?data.black?data.black:'#101010':data.white?data.white:'#FFFFFF'})`);
         window.addEventListener('load',()=>{
             window.document.documentElement.style.removeProperty('background-color');
             if(!window.document.documentElement.style[0])window.document.documentElement.removeAttribute('style');
         });
-        if(window.matchMedia('(prefers-color-scheme:dark)').addEventListener){
+        // theme color
+        if(!(window.navigator.userAgent.match('Safari')&&!window.navigator.userAgent.match('Chrome')&&!window.navigator.userAgent.match('Edg'))&&window.matchMedia('(prefers-color-scheme:dark)').addEventListener){
             const white='#E0E0E0';
             const black='#303030';
             const element=window.document.createElement('meta');
@@ -16,12 +18,14 @@ for(const item of window.document.scripts){
             const theme_color=window.document.head.insertAdjacentElement('beforeend',element);
             window.matchMedia('(prefers-color-scheme:dark)').addEventListener('change',event=>theme_color.content=event.matches?black:white);
         }
+        // script
         if(data.script){
             const element=window.document.createElement('script');
             element.setAttribute('src',data.script);
             element.setAttribute('type','module');
             item.insertAdjacentElement('afterend',element);
         }
+        // clear
         item.parentNode.removeChild(item);
         break;
     }
