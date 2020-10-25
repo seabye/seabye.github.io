3bbec028c493f9b709059b937e2334b1f7c9c125
seabye <seabye@live.com>
2020-10-26 06:00:00 +0800

    ...

--- script.js
+++ script.js
@@ -2,4 +2,7 @@
 import{initial_sw,initial_head}from'/link/initial.js';
 initial_sw('/sw.js');
 initial_head({title:'seabye',style:'/style.css',icon:'/base/icon.png',icon_apple:'/base/icon-apple.png',manifest:'/manifest.webmanifest'});
-window.document.body.insertAdjacentHTML('beforeend','Hello, World!');
+window.document.body.insertAdjacentHTML('beforeend',
+`<div>Hello, World!</div>
+<div>[ <a href="//luojia.me">luojia.me</a> <a href="//pasi.cat">pasi.cat</a> <a href="//oddbye.com">oddbye.com</a> ]</div>`
+);
