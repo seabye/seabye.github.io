6666a1174e3026dcadee7404bab71dd5b2c75c59
seabye <seabye@live.com>
2020-10-26 07:41:21 +0800

    ...

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ initial_sw('/sw.js');
 initial_head({title:'seabye',style:'/style.css',icon:'/base/icon.png',icon_apple:'/base/icon-apple.png',manifest:'/manifest.webmanifest'});
 window.document.body.insertAdjacentHTML('beforeend',
 `<div style="perspective: 50vw;">
-    <div style="font-size: calc(100vw/20); transform: rotateX(-45deg); text-align: center; perspective: 50vw;">
+    <div style="font-size: calc(100vmax/20); transform: rotateX(-45deg); text-align: center; perspective: 50vw;">
         <div style="color: var(--ic_ve_color_black2); transform: rotateY(-45deg);">Hello, World!</div>
         <div style="transform: rotateY(30deg);"><a href="//luojia.me">luojia.me</a></div>
         <div style="transform: rotateY(45deg);"><a href="//pasi.cat">pasi.cat</a></div>
