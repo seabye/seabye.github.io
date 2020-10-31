fe6d0098bae2a48764dbebbc69ca3764a2d7ef44
seabye <seabye@live.com>
2020-10-31 13:34:31 +0800

    ...

--- script.js
+++ script.js
@@ -23,13 +23,13 @@ initial_tool.loop(()=>{
     return result;
 },()=>{
     window.document.body.insertAdjacentHTML('beforeend',
-    `<div style="perspective: 50vw;">
-        <div style="font-size: calc(100vmax/20); transform: rotateX(-45deg); text-align: center; perspective: 50vw;">
-            <div style="color: var(--ic_ve_color_black2); transform: rotateY(-45deg);">Hello, World!</div>
-            <div style="transform: rotateY(30deg);"><a href="//luojia.me">luojia.me</a></div>
-            <div style="transform: rotateY(45deg);"><a href="//pasi.cat">pasi.cat</a></div>
-            <div style="transform: rotateY(60deg);"><a href="//oddbye.com">oddbye.com</a></div>
-        </div>
-    </div>`
+`<div style="perspective: 50vw;">
+    <div style="font-size: calc(100vmax/20); transform: rotateX(-45deg); text-align: center; perspective: 50vw;">
+        <div style="color: var(--ic_ve_color_black2); transform: rotateY(-45deg);">Hello, World!</div>
+        <div style="transform: rotateY(30deg);"><a href="//luojia.me">luojia.me</a></div>
+        <div style="transform: rotateY(45deg);"><a href="//pasi.cat">pasi.cat</a></div>
+        <div style="transform: rotateY(60deg);"><a href="//oddbye.com">oddbye.com</a></div>
+    </div>
+</div>`
     );
 },1000/60);
