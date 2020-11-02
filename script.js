9ec23122e2122b52e97edc53dfa387dde652c6f9
seabye <seabye@live.com>
2020-11-02 10:00:36 +0800

    ...

--- script.js
+++ script.js
@@ -16,12 +16,12 @@ initial_tool.loop(()=>{
     return result;
 },()=>{
     window.document.body.insertAdjacentHTML('beforeend',
-`<div style="perspective: 50vw;">
-    <div style="font-size: calc(100vmax/20); transform: rotateX(-45deg); text-align: center; perspective: 50vw;">
+`<div style="perspective: 100vw;">
+    <div style="font-size: calc(100vmax/20); transform: rotateX(-15deg); text-align: center; perspective: 50vw;">
         <div style="transform: rotateY(-60deg);"><a href="//seabye.com">seabye.com</a></div>
-        <div style="transform: rotateY(-45deg);"><a href="//github.com/seabye">github.com/seabye</a></div>
+        <div style="font-size: 75%; transform: rotateY(-45deg);"><a href="//github.com/seabye">github.com/seabye</a></div>
         <div style="transform: rotateY(-30deg);"><a href="//w3daze.com">w3daze.com</a></div>
-        <div style="color: var(--ic_ve_color_black2);">Hello, World!</div>
+        <div style="font-size: 125%; color: var(--ic_ve_color_black2);">Hello, World!</div>
         <div style="transform: rotateY(30deg);"><a href="//luojia.me">luojia.me</a></div>
         <div style="transform: rotateY(45deg);"><a href="//pasi.cat">pasi.cat</a></div>
         <div style="transform: rotateY(60deg);"><a href="//oddbye.com">oddbye.com</a></div>
