1cd07623b84677a0245d10324c17cf1199efff93
seabye <seabye@live.com>
2020-12-02 10:34:41 +0800

    ...

--- link/initial.js
+++ link/initial.js
@@ -55,7 +55,7 @@
                 }
                 const build_element=(data,position)=>{
                     for(const item in data){
-                        window.console.log(data[item]);
+                        window.console.log(window.JSON.stringify(data[item]));
                         if(!data[item].element){
                             element[item].element=['div',{class:element[item]}]
                         }
