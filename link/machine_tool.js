0e1d50e256388243cdc82f373ff5b961d3694b13
seabye <seabye@live.com>
2021-02-03 15:02:55 +0800

    ...

--- link/machine_tool.js
+++ link/machine_tool.js
@@ -786,7 +786,7 @@
 // #build
 // #debug
     // machine_tool
-    if(window.document?.documentElement&&window.location.hostname==='localhost'){
+    if(window.document?.documentElement&&window.location.hostname.match(/localhost|seabye.com/)){
         (async()=>{
             window.console.log(await machine_tool.aw_loop(
                 ()=>{
