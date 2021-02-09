ecf5118290450ff0d42017d695bc661a39cfd0e9
seabye <seabye@live.com>
2021-02-09 15:25:07 +0800

    ...

---
+++ link/machine_tool_.js
@@ -0,0 +1,296 @@
+import{machine_tool}from'./machine_tool.js';
+export const _={
+    _start(){
+        window.console.log('<<<< start test');
+    },
+    async aw_loop(){
+        // window.console.log(await machine_tool.aw_loop(
+        //     ()=>{
+        //         return false;
+        //     },
+        //     ()=>{
+        //         window.console.log('aw_loop()','1');
+        //         return 11;
+        //     },
+        //     1000,
+        //     2,
+        //     ()=>{
+        //         window.console.log('aw_loop()','2');
+        //         return 22;
+        //     }
+        // ));
+    },
+    async time_out(){
+        // window.console.log('time_out()');
+        // await machine_tool.time_out(()=>{
+        //     window.console.log('time_out()','2000');
+        // },2000);
+    },
+    uuid_36_to_uuid_22(){
+        window.console.log('uuid_36_to_uuid_22()',machine_tool.uuid_36_to_uuid_22('8ef65ee9-a039-4bf2-a4b3-687fcc1f3cc3'));
+    },
+    uuid_22_to_uuid_36(){
+        window.console.log('uuid_22_to_uuid_36()',machine_tool.uuid_22_to_uuid_36('jvZe6aA5S_Kks2h_zB88ww'));
+    },
+    string_to_base64_url_safe_no_pad(){
+        window.console.log('string_to_base64_url_safe_no_pad()',machine_tool.string_to_base64_url_safe_no_pad('8ef65ee9-a039-4bf2-a4b3-687fcc1f3cc3'));
+    },
+    base64_url_safe_no_pad_to_string(){
+        window.console.log('base64_url_safe_no_pad_to_string()',machine_tool.base64_url_safe_no_pad_to_string('jvZe6aA5S_Kks2h_zB88ww'));
+    },
+    create_element(){
+        machine_tool.create_element({
+            '':{},
+            ' _':{}
+        },window.document.body);
+    },
+    switch_state(){
+        this.elements=machine_tool.create_element({
+            target_mode:{
+                element:[,{style:'z-index: 1; position: fixed; left: 0; top: 0; width: 128px; height: 192px; background-color: gray; display: flex; justify-content: center; align-items: center; flex-direction: column;'}],
+                function(elements){
+                    machine_tool.switch_state([
+                        'target',
+                        [
+                            'open_state',
+                            ,
+                            [
+                                elements.target,
+                                elements.target2
+                            ],
+                            [
+                                [
+                                    false,
+                                    'auto',
+                                    [
+                                        elements.button,
+                                        elements.button4
+                                    ],
+                                    'pointer_up',
+                                    (data)=>{
+                                        window.console.log('switch_state()','1 4 pointer_up',data);
+                                    },
+                                    ,
+                                    0
+                                ],
+                                [
+                                    false,
+                                    'open',
+                                    [
+                                        elements.button2
+                                    ],
+                                    'pointer_up',
+                                    (data)=>{
+                                        window.console.log('switch_state()','2 pointer_up',data);
+                                    },
+                                    ,
+                                    0
+                                ],
+                                [
+                                    true,
+                                    'close',
+                                    [
+                                        elements.button3
+                                    ],
+                                    'pointer_up',
+                                    (data)=>{
+                                        window.console.log('switch_state()','3 pointer_up',data);
+                                    },
+                                    ,
+                                    0
+                                ]
+                            ]
+                        ]
+                    ]);
+                },
+                target:{
+                    element:[,{style:'width: 100%; height: 32px; background-color: white;'}]
+                },
+                target2:{
+                    element:[,{style:'width: 100%; height: 32px; background-color: white;'}]
+                },
+                button:{
+                    element:[,{style:'width: 100%; height: 32px; background-color: darkgray;'}]
+                },
+                button2:{
+                    element:[,{style:'width: 100%; height: 32px; background-color: darkred;'}]
+                },
+                button3:{
+                    element:[,{style:'width: 100%; height: 32px; background-color: darkblue;'}]
+                },
+                button4:{
+                    element:[,{style:'width: 100%; height: 32px; background-color: darkgray;'}]
+                }
+            },
+            tab_mode:{
+                element:[,{style:'z-index: 1; position: fixed; left: 128px; top: 0; width: 128px; height: 192px; background-color: gray; display: flex; justify-content: center; align-items: center; flex-direction: column;'}],
+                function(elements){
+                    machine_tool.switch_state([
+                        'tab',
+                        [
+                            'open_state',
+                            ,
+                            [
+                                [
+                                    true,
+                                    [
+                                        elements.red_target,
+                                        elements.red_target2
+                                    ],
+                                    [
+                                        elements.red_button,
+                                        elements.red_button2
+                                    ],
+                                    'pointer_up',
+                                    (data)=>{
+                                        window.console.log('switch_state()','red pointer_up',data);
+                                    },
+                                    ,
+                                    0
+                                ],
+                                [
+                                    false,
+                                    [
+                                        elements.green_target,
+                                        elements.green_target2
+                                    ],
+                                    [
+                                        elements.green_button,
+                                        elements.green_button2
+                                    ],
+                                    'pointer_up',
+                                    (data)=>{
+                                        window.console.log('switch_state()','green pointer_up',data);
+                                    },
+                                    ,
+                                    0
+                                ],
+                                [
+                                    false,
+                                    [
+                                        elements.blue_target,
+                                        elements.blue_target2
+                                    ],
+                                    [
+                                        elements.blue_button,
+                                        elements.blue_button2
+                                    ],
+                                    'pointer_up',
+                                    (data)=>{
+                                        window.console.log('switch_state()','blue pointer_up',data);
+                                    },
+                                    ,
+                                    0
+                                ]
+                            ]
+                        ]
+                    ]);
+                },
+                red_target:{
+                    element:[,{style:'width: 100%; height: 16px; background-color: white;'}]
+                },
+                red_target2:{
+                    element:[,{style:'width: 75%; height: 16px; background-color: white;'}]
+                },
+                red_button:{
+                    element:[,{style:'width: 50%; height: 16px; background-color: darkgray;'}]
+                },
+                red_button2:{
+                    element:[,{style:'width: 25%; height: 16px; background-color: darkgray;'}]
+                },
+                green_target:{
+                    element:[,{style:'width: 100%; height: 16px; background-color: white;'}]
+                },
+                green_target2:{
+                    element:[,{style:'width: 75%; height: 16px; background-color: white;'}]
+                },
+                green_button:{
+                    element:[,{style:'width: 50%; height: 16px; background-color: darkgray;'}]
+                },
+                green_button2:{
+                    element:[,{style:'width: 25%; height: 16px; background-color: darkgray;'}]
+                },
+                blue_target:{
+                    element:[,{style:'width: 100%; height: 16px; background-color: white;'}]
+                },
+                blue_target2:{
+                    element:[,{style:'width: 75%; height: 16px; background-color: white;'}]
+                },
+                blue_button:{
+                    element:[,{style:'width: 50%; height: 16px; background-color: darkgray;'}]
+                },
+                blue_button2:{
+                    element:[,{style:'width: 25%; height: 16px; background-color: darkgray;'}]
+                }
+            }
+        },window.document.body);
+    },
+    listen_element(){
+        machine_tool.listen_element('add',window.document.documentElement,'pointerup',()=>{
+            window.console.log('listen_element()','pointerup');
+        });
+        machine_tool.listen_element('add',window.document.documentElement,'pointer_up',()=>{
+            window.console.log('listen_element()','pointer_up');
+        },undefined,0);
+        // machine_tool.listen_element('add',window.document.documentElement,'pointer_track',()=>{
+        //     window.console.log('listen_element()','pointer_track');
+        // });
+        machine_tool.listen_element('add',this.elements.target,'observe_mutation',()=>{
+            window.console.log('listen_element()','observe_mutation');
+            if(this.elements.target.classList.contains('open_state')){
+                machine_tool.switch_state(this.elements.target_mode,'open_state','_',true);
+            }else{
+                if(this.elements.target.classList.contains('_')){
+                    machine_tool.switch_state(this.elements.target_mode,'_','open_state',true);
+                }
+            }
+        },{attributes:true,attributeFilter:['class'],childList:false,subtree:false});
+        // machine_tool.listen_element('remove',this.elements.target,'observe_mutation',()=>{
+        //     window.console.log('listen_element()','observe_mutation');
+        //     if(this.elements.target.classList.contains('open_state')){
+        //         machine_tool.switch_state(this.elements.target_mode,'open_state','close_state',true);
+        //     }else{
+        //         if(this.elements.target.classList.contains('close_state')){
+        //             machine_tool.switch_state(this.elements.target_mode,'close_state','open_state',true);
+        //         }
+        //     }
+        // },{attributes:true,attributeFilter:['class'],childList:false,subtree:false});
+            machine_tool.listen_element('add',this.elements.red_target,'observe_mutation',()=>{
+                window.console.log('listen_element()','observe_mutation');
+                if(this.elements.target.classList.contains('open_state')){
+                    machine_tool.switch_state(this.elements.target_mode,'open_state','close_state',true);
+                }else{
+                    if(this.elements.target.classList.contains('close_state')){
+                        machine_tool.switch_state(this.elements.target_mode,'close_state','open_state',true);
+                    }
+                }
+            },{attributes:true,attributeFilter:['class'],childList:false,subtree:false});
+                machine_tool.listen_element('remove',this.elements.red_target,'observe_mutation',()=>{
+                    window.console.log('listen_element()','observe_mutation');
+                    if(this.elements.target.classList.contains('open_state')){
+                        machine_tool.switch_state(this.elements.target_mode,'open_state','close_state',true);
+                    }else{
+                        if(this.elements.target.classList.contains('close_state')){
+                            machine_tool.switch_state(this.elements.target_mode,'close_state','open_state',true);
+                        }
+                    }
+                },{attributes:true,attributeFilter:['class'],childList:false,subtree:false});
+        // machine_tool.listen_element('add',window.document.documentElement,'observe_intersection',(data)=>{
+        //     window.console.log('listen_element()','observe_intersection',data);
+        // },{});
+        machine_tool.listen_element('add',window.document.documentElement,'observe_resize',(data)=>{
+            window.console.log('listen_element()','observe_resize',data);
+        });
+    },
+    listen_url(){
+        machine_tool.listen_url('add',(data)=>{
+            window.console.log('listen_url()',data);
+        });
+        // window.history.pushState(null,null,'/x/');
+        // window.history.pushState(null,null,'/');
+        window.history.replaceState(null,null,window.location.pathname);
+    },
+    _end(){
+        window.console.log('>>>> end test');
+    }
+};
