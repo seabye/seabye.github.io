3ce2e0bdf231432713da7ee85e2e940bef44ba21
seabye <seabye@live.com>
2021-02-17 22:41:41 +0800

    ...

--- script.js
+++ script.js
@@ -19,29 +19,34 @@
                     <div style="perspective: 100vw;">
                         <div style="font-size: calc(100vmax/25); transform: rotateX(-15deg); text-align: center; perspective: 50vw;">
                             <div style="transform: rotateY(-60deg);">
-                                <a href="//seabye.com">💧seabye.com</a>
-                                <div style="color: var(--ic_ve_color_black4);">
-                                    <span>❄️</span>
-                                    <a href="//seabye.com/link/gui_initial.js">guii.j</a>
-                                    <a href="//seabye.com/link/gui_initial.css">guii.c</a>
-                                    <a href="//seabye.com/link/machine_tool.js">mt.j</a>
+                                <a href="//seabye.com">seabye.com</a>
+                                <div style="color: var(--ic_ve_color_black2);">
+                                    <span style="font-size: 50%;">gui_initial</span>
+                                    <a href="//seabye.com/link/gui_initial.css">.css</a>
+                                    <a href="//seabye.com/link/gui_initial.js">.js</a>
+                                    <span style="font-size: 38.2%;">🟢90%</span>
+                                    <div>
+                                        <a href="//seabye.com/link/machine_tool.js">machine_tool.js</a>
+                                        <span style="font-size: 38.2%;">🟠50%</span>
+                                    </div>
                                 </div>
                             </div>
                             <div style="font-size: 75%; color: var(--ic_ve_color_black4); transform: rotateY(-45deg);">
-                                <a href="//github.com/seabye">❄️github.com/seabye</a>
+                                <a href="//github.com/seabye">github.com/seabye</a>
                             </div>
                             <div style="color: var(--ic_ve_color_black6); transform: rotateY(-30deg);">
-                                <a href="//w3daze.com">💧w3daze.com</a>
+                                <a href="//w3daze.com">w3daze.com</a>
+                                <span style="font-size: 38.2%;">🔴10%</span>
                             </div>
                             <div style="font-size: 125%; color: rgba(255,0,0,0.1875);">Hello, World!</div>
                             <div style="transform: rotateY(30deg);">
-                                <a href="//luojia.me">🧊luojia.me</a>
+                                <a href="//luojia.me">luojia.me</a>
                             </div>
                             <div style="transform: rotateY(45deg);">
-                                <a href="//pasi.cat">🧊pasi.cat</a>
+                                <a href="//pasi.cat">pasi.cat</a>
                             </div>
                             <div style="transform: rotateY(60deg);">
-                                <a href="//oddbye.com">🧊oddbye.com</a>
+                                <a href="//oddbye.com">oddbye.com</a>
                             </div>
                         </div>
                     </div>
