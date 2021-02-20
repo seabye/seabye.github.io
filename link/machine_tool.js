5efeb6e0434f2e3ee2beff8a86fb74ee0929d9fc
seabye <seabye@live.com>
2021-02-20 09:30:05 +0800

    ...

--- link/machine_tool.js
+++ link/machine_tool.js
@@ -449,7 +449,7 @@
             },
             /*🟠*/listen_element(action,element,type,callback,option={},other=''){
                 const match=/[\r\n\s]/g;
-                const id='id_'+this.java_string_hash_code((element!==window&&element!==window.document?this.element_path(element):'')+callback.toString().replace(match,'')).toString().replace(/[^0-9]/g,'');
+                const id='id_'+this.java_string_hash_code((element!==window&&element!==window.document?this.element_path(element):'')+callback.toString().replace(match,'')+this.random()).toString().replace(/[^0-9]/g,'');
                 switch(type){
                     case'pointer_up':
                         {
