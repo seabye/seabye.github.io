33894974cdbee177529c9ace520798bc8828f430
seabye <seabye@live.com>
2021-03-10 06:18:00 +0800

    ...

--- link/gui_initial.js
+++ link/gui_initial.js
@@ -492,11 +492,13 @@
                     },1000/24);
                 };
                 const move=(event_)=>{
-                    if(event.target.classList.contains('ic_active_down')){
-                        event.target.classList.remove('ic_active_down');
-                    }
-                    if(!event.target.classList.contains('ic_active_move')){
-                        event.target.classList.add('ic_active_move');
+                    if(event_.x>=event.x+3||event_.x<=event.x-3||event_.y>=event.y+3||event_.y<=event.y-3){
+                        if(event.target.classList.contains('ic_active_down')){
+                            event.target.classList.remove('ic_active_down');
+                        }
+                        if(!event.target.classList.contains('ic_active_move')){
+                            event.target.classList.add('ic_active_move');
+                        }
                     }
                     if(window.document.elementFromPoint(event_.x||event_.changedTouches[0].clientX,event_.y||event_.changedTouches[0].clientY)!==event.target){
                         if(!event.target.classList.contains('ic_active_outer')){
