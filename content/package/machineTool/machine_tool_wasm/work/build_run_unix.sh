823eb8482f0c58df6a29329fcba727f1bf5cdb7b
seabye <seabye@live.com>
2021-04-17 15:29:57 +0800

    ...

---
+++ content/package/machineTool/machine_tool_wasm/work/build_run_unix.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# https://www.rust-lang.org/tools/install
+
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+cd $(dirname $0)
+cargo build --release
+../target/release/work
